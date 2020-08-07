8b1db87e10c8bacc10defe803d086ae4891da520
MinusAtaraxy <54963944+MinusAtaraxy@users.noreply.github.com>
2020-08-06 20:30:00 -0600

    what is css
    how is css

--- AMQAutoSkip.user.js
+++ AMQAutoSkip.user.js
@@ -1,16 +1,18 @@
 // ==UserScript==
 // @name         AMQ Auto-Skip
 // @namespace    https://github.com/MinusAtaraxy/AMQ_Scripts
-// @version      0.1+0.01
+// @version      0.1 + 0.2 (Not)
 // @description  afk auto skip
 // @author       Ataraxia
 // @match        https://animemusicquiz.com/*
 // @grant        none
 // @require      https://raw.githubusercontent.com/TheJoseph98/AMQ-Scripts/master/common/amqScriptInfo.js
+// @require      https://gist.githubusercontent.com/arantius/3123124/raw/grant-none-shim.js
+
 
 // ==/UserScript==
 
-//IDEK WTF I AM DOING
+//IDEK WTF I AM DOING AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 
     if (document.getElementById('startPage')) {
         return
@@ -21,28 +23,16 @@ let doSkip = false;
 
 
     if(lobby.inLobby || lobby.isSpectator || quiz.gameMode === "Ranked") return;
-    $("#qpAnswerInputContainer")
-           .append($(`<div class ="" id="qpSkipContainer" style="transform: translateX(-100%);
-	transition: transform 0.5s ease-out;width: 60px;"></div>`)
-        .append($(`<div class="qpSkipSection" style="
-
-	border-top-left-radius: 10px;
-	border-bottom-left-radius: 10px;
-	text-align: center;
-	overflow: hidden;
-
-
-	background-color: #424242;
-	-webkit-box-shadow: 0 0 5px rgb(0, 0, 0);
-	box-shadow: 0 0 5px rgb(0, 0, 0);
-	pointer-events: initial;"></div>`)
+    $("#qpSkipContainer")
+           .append($(`<div id="qpAutoSkipContainer"></div>`)
+        .append($(`<div id="qpAutoSkipSection" class="qpSkipSection"></div>`)
                 .append($(`<div class="customCheckbox"></div>`)
-                        .append($(`<input id="slAFKSkip" type="checkbox">`)
+                        .append($(`<input id="slAFK" type="checkbox">`)
                                 .click(function () {
         toggleAutoSkip();
     })
                                )
-                        .append(`<label for="slAFKSkip"><i class="fa fa-check" aria-hidden="true"></i></label>`)
+                        .append(`<label for="slAFKp"><i class="fa fa-check" aria-hidden="true"></i></label>`)
                        )
                 .append(`</div><p>AFK</p><div>`)
 )
@@ -69,3 +59,41 @@ setTimeout(() => { quiz.skipClicked(); }, 100);
 
         gameChat.systemMessage("skipped!!");
 }
+
+GM_addStyle(`
+#qpAutoSkipContainer {
+    overflow: hidden;
+	width: 150px;
+	z-index: -1;
+	position: absolute;
+	height: 100%;
+	pointer-events: none;
+
+}
+
+#qpAutoSkipSection {
+	float: left;
+	border-top-left-radius: 10px;
+	border-bottom-left-radius: 10px;
+	text-align: center;
+	width: 114px;
+	padding-left: 30px;
+	overflow: hidden;
+	transform: translateX(-110%);
+	transition: transform 0.5s ease-out;
+	background-color: #424242;
+	-webkit-box-shadow: 0 0 5px rgb(0, 0, 0);
+	box-shadow: 0 0 5px rgb(0, 0, 0);
+	pointer-events: initial;
+}
+
+#qpSkipContainer:hover #qpAutoSkipSection {
+	 z-index: 10;
+
+}
+
+#qpAutoSkipSection > p {
+	padding-left: 18px;
+}
+
+`);
