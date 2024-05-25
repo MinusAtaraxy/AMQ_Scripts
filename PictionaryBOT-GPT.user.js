d2bf4dd0d15840178b3fada1c6705c5aeedd2e61
Ataraxia <54963944+MinusAtaraxy@users.noreply.github.com>
2024-05-25 17:37:11 -0600

    Update PictionaryBOT-GPT.user.js
    update

--- PictionaryBOT-GPT.user.js
+++ PictionaryBOT-GPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AMQ BOT - idk
 // @namespace    https://github.com/MinusAtaraxy/AMQ_Scripts
-// @version      0.0.1
+// @version      0.0.2
 // @description  auto say rules/instuctions/links for the custom game pictionary
 // @author       Ataraxy
 // @match        https://animemusicquiz.com/*
@@ -43,13 +43,120 @@ new Listener("game chat update", (payload) => {
     });
 }).bindListener();
 
+new Listener("New Player", function(payload){
+    setTimeout(() => {
+        //payload.name
+        //if not guest
+        //give rules
+
+        if (payload.name.startsWith("Guest-")){
+            //guests go away
+            sendChatMessage("Hi @" + payload.name + " this is a custom game, if you're new to AMQ please leave and find another room for a better experience.")
+        }
+        else {
+            //not guest
+            sendChatMessage("Welcome @" + payload.name + " This is a custom gamemode, please be aware of the rules")
+        }
+
+        //debug//
+        roomsize = getSizeofPlayers();
+
+    },1);
+}).bindListener();
+
+new Listener("New Spectator", function (payload) {
+    setTimeout(() => {
+        //payload.name, payload.gamePlayerId
+        //if guest say it's custom game mode
+        //otherwise give rules/commands
+        if (payload.name.startsWith("Guest-")){
+            //guests go away
+            sendChatMessage("Hi @" + payload.name + " this is a custom game, if you're new to AMQ please leave and find another room for a better experience. ")
+        }
+        else {
+            //not guest
+            sendChatMessage("Welcome @" + payload.name + " This is a custom gamemode. Please understand the rules")
+        }
+        //debug//
+
+    },1);
+}).bindListener();
+
+new Listener("Spectator Change To Player", function(){
+    setTimeout(() => {
+        //remind to remove list and mute sound
+        let oldroomsize = roomsize
+        roomsize = getSizeofPlayers();
+
+        if (roomsize > oldroomsize){
+            sendChatMessage("Pelase make sure you understand the rules. Mute your sound and turn off your list please.")
+        }
+        else {
+            sendChatMessage("Remember to turn off your list and mute sound.")
+        }
+
+    },1);
+}).bindListener();
+
+let joinLobbyListener = new Listener("Join Game", (payload) => {
+    setTimeout(() => {
+        //clear + initialize all stuff
+        //if quiz or if lobby
+        roomsize = getSizeofPlayers();
+
+        if (lobby.inLobby) {
+            //current host
+            // getHostChange();
+
+            //queue player list probably
+        } else {
+            //shouldnt join in game but w/e
+
+        }
+
+    },1);
+}).bindListener();
+
+//when a game is started
+let quizReadyListener = new Listener("quiz ready", (data) => {
+    setTimeout(() => {
+        //give drawing link
+        //queue?
+
+        callChatGPTAPI("encourage everyone to have a great time playing")
+
+
+    },1);
+}).bindListener();
+
+let quizOverListener = new Listener("quiz over", (data) => {
+    setTimeout(() => {
+        //"Please pass host to the next drawer" + queue?
+        //dont forget to remove list
+        callChatGPTAPI("encourage everyone that enjoyed a great game and had fun, encourage artists of their amazing art")
+
+    },1);
+}).bindListener();
+
+function getSizeofPlayers() {
+
+    if (lobby.inLobby){
+        return Object.values(lobby.players)[Object.values(lobby.players).length-1].gamePlayerId;
+        }
+    else {
+        return Object.values(quiz.players)[Object.values(quiz.players).length-1].gamePlayerId;
+    }
+
+
+}
+
 
 function processChatCommand(payload) {
 
     //    console.log(payload);
     //    gameChat.systemMessage("ok")
     if (payload.sender !== selfName){
-        callChatGPTAPI(payload);
+        callChatGPTAPI(payload.sender + ": " + payload.message);
     }
 
 }
@@ -75,33 +182,17 @@ async function callChatGPTAPI(prompt) {
         body: JSON.stringify({
             messages: [
                 {
-                    role: "user",
-                    content: `The AI is in charge of moderating the chatroom consisting of multiple users. This chatroom was created so the users can play a game of pictionary. The AI must provide the following information when asked: The url to play the game is at "https://magma.com/d/12MP66p5kG". The rules of the game must be followed as such: "1. turn off your list", "2. mute your sound", "3. guess the anime using drawings only".`
+                    role: "assisstant",
+                    content: `The AI is in charge of moderating the chat consisting of multiple users. This chatroom was created so the users can play a game of pictionary, messages will start with the username of the current person talking. There is a 300 character limit in your response. The AI must provide the following information when asked: The url to play the game is at "https://magma.com/d/12MP66p5kG". The rules of the game must be followed as such: "1. turn off your list", "2. mute your sound", "3. guess the anime using drawings only".`
       },
             {
                 role: "user",
-                content: prompt.message
+                content: prompt
             }
         ],
-//         functions: [
-//             {name: "get_gamemode_rules",
-//              description: "provide the rules for the gamemode if someone asks",
-//              parameters: {
-//                  type: "object",
-//                  properties: {
-//                      rules:{
-//                          type: "string",
-//                          description: "1. turn off your list, 2. mute your sound, 3. guess the anime using drawings only",
-//                      },
-//                  },
-//                  required: ["rules"]
-//              }
-//             }
-//         ],
         mode: "chat",
         instruction_template: "Alpaca",
         temperature: 0.7,
-        user: prompt.sender,
         character: "PictionaryBOT"
     }),
   });
@@ -109,10 +200,34 @@ async function callChatGPTAPI(prompt) {
     if (response.ok) {
         const data = await response.json();
         console.log(data.choices[0].message.content);
-        sendChatMessage(data.choices[0].message.content)
+        if (data.choices[0].message.content.length > 150){
+            let splits = splitString (150,data.choices[0].message.content)
+            splits.forEach(sendChatMessage);
+        }
+        else{
+            sendChatMessage(data.choices[0].message.content)
+        }
     } else {
         console.error('Error calling ChatGPT API:', response.statusText);
     }
+}
+
+function splitString (n,str){
+    let arr = str?.split(' ');
+    let result=[]
+    let subStr=arr[0]
+    for(let i = 1; i < arr.length; i++){
+        let word = arr[i]
+        if(subStr.length + word.length + 1 <= n){
+            subStr = subStr + ' ' + word
+        }
+        else{
+            result.push(subStr);
+            subStr = word
+        }
+    }
+    if(subStr.length){result.push(subStr)}
+    return result
 }
 
 /*
